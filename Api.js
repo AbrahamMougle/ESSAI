31de0f056e6fb04b5dc2ee0142655608d2d0553f
abraham <abrahammougle76@gmail.com>
2024-03-17 02:43:01 +0100

    'fin'

--- Api.js
+++ Api.js
@@ -1,1 +1,16 @@
-Commande ECHO activ‚e.
+let button = document.getElementById("button")
+let image = document.getElementById("image")
+let pokeNumber= document.getElementById("number")
+let pokeName = document.getElementById("name")
+const changePokemon=async ()=>{
+    let randoomNumber = Math.ceil(Math.random() * 151)+1;
+    let requestString=`https://pokeapi.co/api/v2/pokemon/${randoomNumber}`
+  let data = await fetch(requestString);
+    let response = await data.json()
+    console.log(response)
+    image.src =response.sprites.front_default
+    pokeName.textContent=response.name;
+    pokeNumber.textContent=response.id
+}
+changePokemon();
+button.addEventListener("click",changePokemon)
