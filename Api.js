0c171645941e38fec790ca3455b7476a2461145a
WilsonTAb <abrahammougle76@gmail.com>
2024-04-01 22:04:16 +0200

    'redisign'

--- Api.js
+++ Api.js
@@ -1,16 +1,28 @@
-let button = document.getElementById("button")
+let nextButton = document.querySelector(".buttonNext")
+let prevButton = document.querySelector(".buttonPrev")
 let image = document.getElementById("image")
 let pokeNumber= document.getElementById("number")
 let pokeName = document.getElementById("name")
+let prev=1;
 const changePokemon=async ()=>{
-    let randoomNumber = Math.ceil(Math.random() * 151)+1;
-    let requestString=`https://pokeapi.co/api/v2/pokemon/${randoomNumber}`
-  let data = await fetch(requestString);
+    let requestString=`https://pokeapi.co/api/v2/pokemon/${prev}`
+    prev++;
+    let data = await fetch(requestString);
     let response = await data.json()
-    console.log(response)
     image.src =response.sprites.front_default
     pokeName.textContent=response.name;
     pokeNumber.textContent=response.id
 }
+const changePrev=async ()=>{
+  let requestString=`https://pokeapi.co/api/v2/pokemon/${prev}`
+  prev--;
+  let data = await fetch(requestString);
+  let response = await data.json()
+  image.src =response.sprites.front_default
+  pokeName.textContent=response.name;
+  pokeNumber.textContent=response.id
+}
+
 changePokemon();
-button.addEventListener("click",changePokemon)
+nextButton.addEventListener("click",changePokemon)
+prevButton.addEventListener('click',changePrev)
